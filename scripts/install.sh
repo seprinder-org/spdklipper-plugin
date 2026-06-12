7bcdbe415f3ead1c781fd2f4b5aaff47885660df
Gia Bao <ngogiabao271202@gmail.com>
2026-06-12 19:58:51 +0700

    refactor: simplify install.sh to single-instance, remove interactive prompts

--- scripts/install.sh
+++ scripts/install.sh
@@ -71,12 +71,7 @@ check_os() {
 
 # Main functions
 init_config_path() {
-  report_status "SPDKlipper plugin configuration file location selection"
-  echo -e "\n"
-  echo "Enter the path for the configuration files location. Subfolders for multiple instances wil be created under this path."
-  echo "Its recommended to store it together with the klipper configuration for easier backup and usage."
-  read -p "Enter desired path: " -e -i "${KLIPPER_CONF_DIR}" klip_conf_dir
-  KLIPPER_CONF_DIR=${klip_conf_dir}
+  report_status "Using default config path: ${KLIPPER_CONF_DIR}"
 
   if ! [ -z ${LPATH+x} ]; then
     KLIPPER_LOGS_DIR=${LPATH}
@@ -86,68 +81,19 @@ init_config_path() {
 }
 
 create_initial_config() {
-  if [[ $INSTANCE_COUNT -eq 1 ]]; then
-    SPDKLIPPER_PLUGIN_CONF=${KLIPPER_CONF_DIR}
-    # check in config exists!
-    if [[ ! -f "${SPDKLIPPER_PLUGIN_CONF}"/spdklipper.conf ]]; then
-      report_status "Creating base config file"
-      if [[ ! -f "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template ]]; then
-        warn_msg "Base install template not found at ${SPDKLIPPER_PLUGIN_DIR}/scripts/base_install_template"
-        return 1
-      fi
-      cp -n "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template "${SPDKLIPPER_PLUGIN_CONF}"/spdklipper.conf
-
+  SPDKLIPPER_PLUGIN_CONF=${KLIPPER_CONF_DIR}
+  # check if config exists!
+  if [[ ! -f "${SPDKLIPPER_PLUGIN_CONF}"/spdklipper.conf ]]; then
+    report_status "Creating base config file"
+    if [[ ! -f "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template ]]; then
+      warn_msg "Base install template not found at ${SPDKLIPPER_PLUGIN_DIR}/scripts/base_install_template"
+      return 1
     fi
-
-    create_service
-    ok_msg "Single Spdklipper plugin instance created!"
-
-  else
-    manual_paths=""
-    while [[ ! ($manual_paths =~ ^(?i)(y|n|no|yes)(?-i)$) ]]; do
-      read -p "Use automatic paths? (Y/n): " -e -i y manual_paths
-      case "${manual_paths}" in
-        Y|y|Yes|yes)
-          echo -e "###### > Yes"
-          manual_paths="y"
-          break;;
-        N|n|No|no)
-          echo -e "###### > No"
-          manual_paths="n"
-          break;;
-        *)
-          warn_msg "Invalid command!";;
-      esac
-    done
-    i=1
-    while [[ $i -le $INSTANCE_COUNT ]]; do
-      ### rewrite default variables for multi instance cases
-      if [ "${manual_paths}" == "n" ]; then
-        report_status "SPDKlipper plugin instance name selection for instance ${i}"
-        read -p "Enter plugin instance name: " -e -i "printer_${i}" instance_name
-        SPDKLIPPER_PLUGIN_SERVICE="spdklipper-plugin-${instance_name}.service"
-        SPDKLIPPER_PLUGIN_CONF="${KLIPPER_CONF_DIR}/${instance_name}"
-        SPDKLIPPER_PLUGIN_LOG="${KLIPPER_LOGS_DIR}/spdklipper-${instance_name}.log"
-      else
-        SPDKLIPPER_PLUGIN_SERVICE="spdklipper-plugin-$i.service"
-        SPDKLIPPER_PLUGIN_CONF="${KLIPPER_CONF_DIR}/printer_$i"
-        SPDKLIPPER_PLUGIN_LOG="${KLIPPER_LOGS_DIR}/spdklipper-$i.log"
-      fi
-
-      report_status "Creating base config file"
-      mkdir -p "${SPDKLIPPER_PLUGIN_CONF}"
-      if [[ ! -f "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template ]]; then
-        warn_msg "Base install template not found at ${SPDKLIPPER_PLUGIN_DIR}/scripts/base_install_template"
-        return 1
-      fi
-      cp -n "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template "${SPDKLIPPER_PLUGIN_CONF}"/spdklipper.conf
-      mkdir -p "${KLIPPER_LOGS_DIR}"
-      create_service
-      ### raise values by 1
-      i=$((i+1))
-    done
-    unset i
+    cp -n "${SPDKLIPPER_PLUGIN_DIR}"/scripts/base_install_template "${SPDKLIPPER_PLUGIN_CONF}"/spdklipper.conf
   fi
+
+  create_service
+  ok_msg "SPDKlipper plugin instance created!"
 }
 
 #Todo: stop multiple?
@@ -515,8 +461,6 @@ EOF
 
 
 install_instances(){
-  INSTANCE_COUNT=$1
-
   sudo systemctl stop spdklipper-plugin* || true
   status_msg "Installing dependencies"
   install_packages
@@ -663,54 +607,9 @@ restart_services() {
 
 
 setup_dialog(){
-    ### count amount of moonraker services
-    SERVICE_FILES=$(find "$SYSTEMDDIR" -regextype posix-extended -regex "$SYSTEMDDIR/spdklipper-plugin(-[a-zA-Z0-9]+)*.service" 2>/dev/null || true)
-    if [ -f /etc/init.d/moonraker ] || [ -f /etc/systemd/system/moonraker.service ]; then
-      MOONRAKER_COUNT=1
-    elif [ -n "$SERVICE_FILES" ]; then
-      MOONRAKER_COUNT=$(echo "$SERVICE_FILES" | wc -l)
-    else
-      MOONRAKER_COUNT=0
-    fi
-
-    echo -e "/=======================================================\\"
-    if [[ $MOONRAKER_COUNT -eq 0 ]]; then
-      printf "|${yellow}%-55s${default}|\n" " No Moonraker instance was found!"
-    elif [[ $MOONRAKER_COUNT -eq 1 ]]; then
-      printf "|${green}%-55s${default}|\n" " 1 Moonraker instance was found!"
-    elif [[ $MOONRAKER_COUNT -gt 1 ]]; then
-      printf "|${green}%-55s${default}|\n" "${MOONRAKER_COUNT} Moonraker instances were found!"
-    else
-      echo -e "| ${yellow}INFO: No existing Moonraker installation found!${default}        |"
-      init_config_path
-    fi
-    echo -e "| Usually you need one SPDKlipper plugin instance per Moonraker   |"
-    echo -e "| instance. Though you can install as many as you wish. |"
-    echo -e "\=======================================================/"
-    echo
-    count=""
-    while [[ ! ($count =~ ^[1-9]+((0)+)?$) ]]; do
-      read -p "${cyan}###### Number of SPDKlipper plugin instances to set up:${default} " count
-      if [[ ! ($count =~ ^[1-9]+((0)+)?$) ]]; then
-        echo -e "Invalid Input!\n"
-      else
-        echo
-        read -p "${cyan}###### Install $count instance(s)? (Y/n):${default} " yn
-        case "$yn" in
-          Y|y|Yes|yes|"")
-            echo -e "###### > Yes"
-            status_msg "Installing SPDKlipper plugin ...\n"
-            install_instances "$count"
-            break;;
-          N|n|No|no)
-            echo -e "###### > No"
-            warn_msg "Exiting SPDKlipper plugin setup ...\n"
-            break;;
-          *)
-            warn_msg "Invalid command!";;
-        esac
-      fi
-    done
+    report_status "Starting SPDKlipper plugin installation..."
+    echo ""
+    install_instances
 }
 
 
